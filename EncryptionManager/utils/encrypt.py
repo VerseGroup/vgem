9520704a9c43b4691dfa6118a6f2ea9ee5a0dbea
Paul Evans <pauljevans927@gmail.com>
2022-01-13 11:10:24 -0500

    added encryption functions for both private/public keys

--- EncryptionManager/utils/encrypt.py
+++ EncryptionManager/utils/encrypt.py
@@ -2,3 +2,26 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+def encrypt_private(message, private_key):
+    message = message.encode('utf-8') # encrypting requires binary
+    encrypted_message = private_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+        )
+    return encrypted_message
+
+def encrypt_public(message, public_key):
+    message = message.encode('utf-8') # encrypting requires binary
+    encrypted_message = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+        )
+    return encrypted_message
