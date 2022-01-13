ce402039c3f75efaaed99ce822dc8ae850d033ce
Paul Evans <pauljevans927@gmail.com>
2022-01-13 11:34:29 -0500

    added all imports to init for shorter routes

---
+++ RSA/utils/__init__.py
@@ -0,0 +1,15 @@
+# python imports
+import os
+import sys
+
+# adding dir to sys to allow local importing
+currentdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(currentdir)
+
+# adding local imports to allow shorter imports
+from base64ec import encode, decode
+from decrypt import decrypt_private
+from encrypt import encrypt_private, encrypt_public
+from serialize import serialize_private_key, serialize_public_key
+from deserialize import deserialize_private_key, deserialize_public_key
+from generate import generate_private, generate_public
