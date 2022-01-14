a37effdf4b85d955914a1389650e17c89b416bdb
Paul Evans <pauljevans927@gmail.com>
2022-01-14 10:23:36 -0500

    renaming class to class_ because class is reserved

---
+++ EM-RSA/__init__.py
@@ -0,0 +1,11 @@
+# python imports
+from curses.ascii import EM
+import os
+import sys
+
+# adding dir to sys to allow local importing
+currentdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(currentdir)
+
+# local imports 
+from class_ import EM
