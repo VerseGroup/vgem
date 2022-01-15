8f9a6a1c5fa447397c5c11bbd7fd13baafb3e25f
Paul Evans <pauljevans927@gmail.com>
2022-01-15 10:36:36 -0500

    testing routing works

---
+++ tests/em-testing.py
@@ -0,0 +1,13 @@
+# python imports
+import os
+import sys
+
+# adding dir to sys to allow local importing
+currentdir = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(currentdir)
+parentdir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parentdir)
+
+# local imports
+from src import EM
+
